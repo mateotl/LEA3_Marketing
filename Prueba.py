6c141e64f9c192fabf9fe3b525cf6de98fd91d56
mateotl <mateo.toro1@udea.edu.co>
2025-03-18 07:31:25 -0500

    Cargar datos

--- Prueba.py
+++ Prueba.py
@@ -1,3 +1,10 @@
 print("hola")
 ### Santiago
 print("adios")
+
+import pandas as pd
+
+df_read = 'https://raw.githubusercontent.com/mateotl/LEA3_Seminario/6c97255963fc9d1f001fb3e74aaaef7762cbc2cf/aug_test.csv'
+df = pd.read_csv(df_read, sep = ',')
+
+df.head()
