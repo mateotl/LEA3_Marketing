6dce92fe72cba5806af9f1cacb47b7df50f21fb9
mateotl <mateo.toro1@udea.edu.co>
2025-05-10 13:25:20 -0500

    Actualización

--- preprocesamientos.sql
+++ preprocesamientos.sql
@@ -2,24 +2,25 @@
 
 --- Crear tabla con los usuarios que tienen entre 30 y 300 ratings---
 
-drop table if exists usuarios_selectos;
+DROP TABLE IF EXISTS usuarios_selectos;
 
-create table usuarios_selectos
-select userId AS user_id, COUNT(*) AS cnt_rat
-from ratings
-group by userId
-having cnt_rat >30 AND cnt_rat < 300
-Order by cnt_rat desc;
+CREATE TABLE usuarios_selectos AS
 
----Crear tabla con las peliculas que tienen mas de 10 ratings---
-drop table if exists Pelis_selectas;
+SELECT userId AS user_id, COUNT(*) AS cnt_rat
+FROM ratings
+GROUP BY userId
+HAVING cnt_rat > 30 AND cnt_rat < 300;
 
-create table Pelis_selectas
-select movieId AS movie_id, COUNT(*) AS cnt_rat
-from ratings
-group by movieId
-having cnt_rat > 10
-Order by cnt_rat desc;
+-- Crear tabla con las películas que tienen más de 10 ratings
+DROP TABLE IF EXISTS Pelis_selectas;
+
+CREATE TABLE Pelis_selectas AS
+
+SELECT movieId AS movie_id, COUNT(*) AS cnt_rat
+FROM ratings
+GROUP BY movieId
+HAVING cnt_rat > 10;
+
 
 ---Crear tablas filtradas de movies y ratings---
 DROP TABLE IF EXISTS ratings_final;
